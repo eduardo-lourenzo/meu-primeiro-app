1a618ecde5f36873e011d67058af96a3c0e7d5c1
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-05-12 15:41:50 -0300

    build: Remove AppModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule, RouterModule]
 })
 export class AppComponent {
-  title = 'meu-primeiro-app';
+  title = 'Meu Primeiro App';
 }
