46920f0b3e0df32a5e9fa72d1d8a9dd84ef8af92
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-05-16 16:44:46 -0300

    feat: create phone-frame component

---
+++ src/app/shared/phone-frame/phone-frame.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-phone-frame',
+  standalone: true,
+  imports: [],
+  templateUrl: './phone-frame.component.html',
+  styleUrls: ['./phone-frame.component.css']
+})
+export class PhoneFrameComponent {
+
+}
