9fc82237e8a158165d5f32ceb1ef38642166a0fb
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-05-20 12:01:16 -0300

    feat: create app-card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PhoneFrameComponent } from 'src/app/shared/phone-frame/phone-frame.component';
+import { CardComponent } from 'src/app/shared/card/card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PhoneFrameComponent],
+  imports: [PhoneFrameComponent, CardComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
