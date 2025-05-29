9898623c517d9b30f6fede3ee8ed8c5f5455666f
Eduardo C. Lourenzo <eduardo.lourenzo@zup.com.br>
2025-05-29 16:14:04 -0300

    feat: remove variable from app-header html

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  private _fullUsername = "Fulano da Silva";
+  private _cpf = "***.456.789-**";
 
+
+  get firstName(): string {
+    return this._fullUsername.split(' ')[0];
+  }
+
+  get initials(): string {
+    const parts = this._fullUsername.trim().split(' ');
+
+    if (parts.length === 0) return "00";
+    if (parts.length === 1) return (parts[0][0] + parts[0][0]).toUpperCase();
+    return (parts[0][0] + parts[parts.length - 1][0]).toUpperCase();
+  }
+  get cpf() : string {
+    return this._cpf;
+  }
 }
